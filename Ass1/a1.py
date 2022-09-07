35dd2e1700d11f24092e969f7badb736e7cb15ac
Salil <salilgokhale24@gmail.com>
2022-09-07 15:33:41 +0530

    Final ass1

--- Ass1/a1.py
+++ Ass1/a1.py
@@ -1,15 +1,17 @@
-
-
 # Stack implementation using a linked list of Nodes
 # ___________________________________________________________________________
 
 class Node:
+    __slots__ = 'data', 'next'
+
     def __init__(self, data=None, next_node=None):
         self.data = data  # data of node
         self.next = next_node  # next node, None is end of stack
 
 
 class Stack:
+    __slots__ = 'head', 'length'
+
     def __init__(self):  # init empty stack
         self.head = Node()
         self.length = 0
@@ -54,7 +56,7 @@ def apply_operation(value_stack, operator_stack):
         value_stack.push([op1[i] + op2[i] for i in range(4)])
     elif operand == '-':
         value_stack.push([op2[0] - op1[0], op2[1] - op1[1],
-                          op2[2] - op1[2], op2[3] + op1[3]])
+                          op2[2] - op1[2], op2[3] + op1[3]])  # distance always added
     else:
         value_stack.push([op2 * op1[i] for i in range(4)])
 
@@ -78,18 +80,18 @@ def add_token(token, value_stack, operator_stack):
         value_stack.push(token)
     elif token == '(':
         operator_stack.push('(')
-        value_stack.push([0, 0, 0, 0])  # add further statements to this value
+        value_stack.push([0, 0, 0, 0])  # add further statements to base value
     elif token == ')':
         while operator_stack.top() != '(':  # apply all operations between brackets
             apply_operation(value_stack, operator_stack)
         operator_stack.pop()
     elif token == '+' or token == '-':
-        # only apply operators wither higher or equal precedence till first bracket
+        # only apply operators with higher or equal precedence till first bracket
         while not operator_stack.is_empty() and operator_stack.top() != '(':
             apply_operation(value_stack, operator_stack)
         operator_stack.push(token)
     elif token == '*':
-        # only apply operators wither higher or equal precedence till first bracket
+        # only apply operators withs higher or equal precedence till first bracket
         while (not operator_stack.is_empty()) and operator_stack.top() == '*':
             apply_operation(value_stack, operator_stack)
         operator_stack.push(token)
@@ -104,13 +106,16 @@ def findPositionandDistance(P):
     while i < length:
         if P[i].isdigit():
             j = i
+            num = 0
             # splice full number from P
             while j < length and P[j].isdigit():
+                num *= 10
+                num += int(P[j])
                 j += 1
             # to indicate the final result after multiplication is to be added
             add_token('+', value_stack, operator_stack)
             # convert the splice to integer
-            add_token(int(P[i:j]), value_stack, operator_stack)
+            add_token(num, value_stack, operator_stack)
             # to indicate multiplication after a number
             add_token('*', value_stack, operator_stack)
             i = j
@@ -127,6 +132,8 @@ def findPositionandDistance(P):
 #     "+X+Y+X-Y-Z+X+X-Z-Z-Z-Z-Y+Y-X") == [3, 0, -5, 14]
 # assert findPositionandDistance("+X2(+Y-X-Z)8(+Y)9(-Z-Z)") == [-1, 10, -20, 33]
 # assert findPositionandDistance("") == [0, 0, 0, 0]
+# assert findPositionandDistance("4()") == [0, 0, 0, 0]
+# assert findPositionandDistance("5(4())") == [0, 0, 0, 0]
 # assert findPositionandDistance("5(2(3(+X+X)))") == [60, 0, 0, 60]
 # assert findPositionandDistance(
 #     "+Z6(+X+Y+X-Y)9(-X+Z-X-Z8(+X+Y-Z)9(+Y-Z-X-Y4(-X+Y-X-Z+X)))") == [-339, 396, -476, 2221]
