c5c8827b79b60888db36e5dbd1ce2d085566be2d
Salil <salilgokhale24@gmail.com>
2022-11-02 14:08:28 +0530

    a4 and a5 complete

--- Ass4/a4.py
+++ Ass4/a4.py
@@ -1,44 +1,105 @@
 import random
 import math
 
-#To generate random prime less than N
+# To generate random prime less than N
+
+
 def randPrime(N):
-	primes = []
-	for q in range(2,N+1):
-		if(isPrime(q)):
-			primes.append(q)
-	return primes[random.randint(0,len(primes)-1)]
+    primes = []
+    for q in range(2, N+1):
+        if (isPrime(q)):
+            primes.append(q)
+    return primes[random.randint(0, len(primes)-1)]
 
 # To check if a number is prime
+
+
 def isPrime(q):
-	if(q > 1):
-		for i in range(2, int(math.sqrt(q)) + 1):
-			if (q % i == 0):
-				return False
-		return True
-	else:
-		return False
+    if (q > 1):
+        for i in range(2, int(math.sqrt(q)) + 1):
+            if (q % i == 0):
+                return False
+        return True
+    else:
+        return False
 
-#pattern matching
-def randPatternMatch(eps,p,x):
-	N = findN(eps,len(p))
-	q = randPrime(N)
-	return modPatternMatch(q,p,x)
+# pattern matching
 
-#pattern matching with wildcard
-def randPatternMatchWildcard(eps,p,x):
-	N = findN(eps,len(p))
-	q = randPrime(N)
-	return modPatternMatchWildcard(q,p,x)
+
+def randPatternMatch(eps, p, x):
+    N = findN(eps, len(p))
+    q = randPrime(N)
+    return modPatternMatch(q, p, x)
+
+# pattern matching with wildcard
+
+
+def randPatternMatchWildcard(eps, p, x):
+    N = findN(eps, len(p))
+    q = randPrime(N)
+    return modPatternMatchWildcard(q, p, x)
 
 # return appropriate N that satisfies the error bounds
-def findN(eps,m):
-	pass
+
+
+def findN(eps, m):
+    '''We want to pick N such that N/ln(N) >= 2*m*log(26)/eps = k(say). This will ensure (number of primes which divide f(A)-f(B))/(total number of primes) <= eps
+
+    It can be proved that if we have m >=1 and eps <1 then choosing N = 2*k*log(2*k) will satisfy the above condition.
+    '''
+    k = math.ceil(2*m * math.log2(26)/eps)
+    return int(math.ceil(2*k*math.log2(2*k)))
+
 
 # Return sorted list of starting indices where p matches x
-def modPatternMatch(q,p,x):
-	pass
+def modPatternMatch(q, p, x):
+    pattern = 0  # holds the number for pattern
+    roll = 0  # holds the number for m digits of x
+    power = 1
+    for i in range(len(p)):
+        pattern = (pattern * 26 + ord(p[i]) - 65) % q
+        roll = (roll * 26 + ord(x[i]) - 65) % q
+        power = (power * 26) % q
+    L = []
+    for i in range(len(x) - len(p) + 1):
+        if pattern == roll:
+            L.append(i)
+        if i < len(x) - len(p):
+            # remove the first digit and add next digit
+            roll = (roll * 26) % q
+            roll = (roll - power * (ord(x[i]) - 65)) % q
+            roll = (roll + ord(x[i + len(p)]) - 65) % q
+    return L
+
 
 # Return sorted list of starting indices where p matches x
-def modPatternMatchWildcard(q,p,x):
-	pass
+def modPatternMatchWildcard(q, p, x):
+    pattern = 0
+    wildcard = 0  # position of wildcard
+    roll = 0
+    power = 1
+    for i in range(len(p)):
+        power = (power * 26) % q
+        if p[i] == '?':
+            pattern = (pattern * 26) % q
+            wildcard = i
+            roll = (roll*26) % q
+        else:
+            pattern = (pattern * 26 + ord(p[i]) - 65) % q
+            roll = (roll * 26 + ord(x[i]) - 65) % q
+
+    power_wild = 1
+    for i in range(len(p) - wildcard-1):
+        power_wild = (power_wild*26) % q
+    L = []
+    for i in range(len(x) - len(p)+1):
+        if pattern == roll:
+            L.append(i)
+        if i+len(p) < len(x):
+            roll = (roll + power_wild*(ord(x[i + wildcard]) - 65)) % q  # add digit at wildcard
+            # remove first digit and add next digit
+            roll = (roll * 26) % q
+            roll = (roll - power * (ord(x[i]) - 65)) % q
+            roll = (roll + ord(x[i + len(p)]) - 65) % q
+            roll = (roll - power_wild*(ord(x[i+wildcard+1]) - 65)) % q  # remove digit at wildcard
+    return L
---
+++ Ass5/a5.py
@@ -0,0 +1,104 @@
+class MinPriorityQueue:
+    '''Minimum Priority Queue Implementation using Min-Heap.
+
+    Heap is stored in an array where left child = 2*i+1 and right child is 2*i+2. This convention is followed to allow 0-indexing'''
+
+    def min_heapify(self, idx):
+        '''send value at idx to correct position and maintain heap property'''
+        minimum = -1
+        left = -1
+        right = -1
+        while idx != minimum:
+            left = 2*idx+1
+            right = 2*idx+2
+            minimum = idx
+            if left < self._size and self._array[left] < self._array[minimum]:
+                minimum = left
+            if right < self._size and self._array[right] < self._array[minimum]:
+                minimum = right
+            if minimum != idx:
+                self._array[minimum], self._array[idx] = self._array[idx], self._array[minimum]
+                idx = minimum
+                minimum = -1
+
+    def __init__(self, array):
+        '''initialize the heap in O(n)
+
+        _size: size of the heap
+        _arraysize: size of the container array
+        _array: reference to container array
+        '''
+        self._arraysize = len(array)
+        self._size = len(array)
+        self._array = array
+        for i in range(self._size//2 - 1, -1, -1):
+            self.min_heapify(i)
+
+    def top(self):
+        '''returns the minimum value in priority queue'''
+        return self._array[0]
+
+    def pop(self):
+        '''return the minimum value and removes it from priority queue'''
+        minimum = self._array[0]
+        self._array[0] = self._array[self._size-1]
+        self._size -= 1
+        if self._size > 0:
+            self.min_heapify(0)
+        return minimum
+
+    def decrease_value(self, idx, value):
+        '''decrease the key at position idx and set it to value'''
+        self._array[idx] = value
+        while idx > 0 and self._array[(idx-1)//2] > self._array[idx]:
+            self._array[(
+                idx-1)//2], self._array[idx] = self._array[idx], self._array[(idx-1)//2]
+            idx = (idx-1)//2
+
+    def insert(self, value):
+        '''insert new element in priority queue'''
+        self._size += 1
+        if self._arraysize >= self._size:
+            self._array[self._size-1] = (float('inf'), -1, -1,)
+        else:
+            self._array.append((float('inf'), -1, -1,))
+            self._arraysize += 1
+        self.decrease_value(self._size-1, value)
+
+    def is_empty(self):
+        return (self._size == 0)
+
+# __________________________________________________________________________________________________________
+
+
+def findMaxCapacity(n, links, s, t):
+    graph = [[] for i in range(n)]  # stores the graph in an adjacency list
+    INT_MAX = links[0][2]  # holds the value of inf, calculated by max of all edges +10
+    for u, v, cap in links:  # build adjacency list
+        graph[u].append((v, cap))
+        graph[v].append((u, cap))
+        INT_MAX = max(cap, INT_MAX)
+    MaxCapacity = [-1] * n  # holds the value of maximum packet size from source to ith node
+    MaxCapacity[s] = INT_MAX + 10
+    backtrack = [-1] * n  # holds the parent of most optimum path according to Dijkstra's
+    visited = [False]*n  # true if node is already visited
+    pqueue = MinPriorityQueue([(-INT_MAX - 10, s)])  # insert negative capacities because its a min-priority queue
+    while not pqueue.is_empty():
+        curr = pqueue.pop()[1]
+        if curr == t:  # once t is selected, its MaxCapacity is final. so we can break
+            break
+        if visited[curr]:
+            continue
+        visited[curr] = True
+        for node, capacity in graph[curr]:
+            if MaxCapacity[node] < min(MaxCapacity[curr], capacity):  # contract the path
+                MaxCapacity[node] = min(MaxCapacity[curr], capacity)
+                pqueue.insert((-MaxCapacity[node], node))
+                backtrack[node] = curr
+    # restore optimum path
+    node = t
+    path = []
+    while node != -1:
+        path.append(node)
+        node = backtrack[node]
+    return MaxCapacity[t], path[::-1]  # path is restored in reverse order
